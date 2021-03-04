09afb9b3d642242063ae6a553e2cc9ff9a514436
이종호 <bazinga940@gmail.com>
2021-03-04 14:32:38 +0900

    create xcode framework project

---
+++ BottomSheet/BottomSheet/BottomSheet.h
@@ -0,0 +1,19 @@
+//
+//  BottomSheet.h
+//  BottomSheet
+//
+//  Created by 60067671 on 2021/03/04.
+//  Copyright © 2021 JH. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BottomSheet.
+FOUNDATION_EXPORT double BottomSheetVersionNumber;
+
+//! Project version string for BottomSheet.
+FOUNDATION_EXPORT const unsigned char BottomSheetVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BottomSheet/PublicHeader.h>
+
+
