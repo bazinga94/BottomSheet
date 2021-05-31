a57c15c3828a8d2fa1854f42234302d6076e16d2
이종호 <bazinga940@gmail.com>
2021-05-31 13:48:22 +0900

    바텀시트에서 네비게이션 push를 할 수 있을지 확인 중

--- Demo/Demo/FlexibleSampleViewController.swift
+++ Demo/Demo/FlexibleSampleViewController.swift
@@ -13,4 +13,13 @@ class FlexibleSampleViewController: UIViewController, FlexibleBottomSheetDelegat
 	@IBAction func closeBottomSheet(_ sender: Any) {
 		bottomSheet?.dismissSheet()
 	}
+
+	@IBAction func navigationPush(_ sender: Any) {
+		let storyBoard: UIStoryboard! = UIStoryboard(name: "Main", bundle: nil)
+		let vc = storyBoard.instantiateViewController(withIdentifier: "TableViewSheet") as! TableViewSampleViewController
+//		self.navigationController?.pushViewController(vc, animated: true)
+		if let navigationController = bottomSheet?.presentingViewController as? UINavigationController {
+			navigationController.pushViewController(vc, animated: true)
+		}
+	}
 }
