50ac12df7b6ce3fab03ca17b8cab543c4987a2bd
이종호 <bazinga940@gmail.com>
2021-08-24 09:39:42 +0900

    edit platforms version

--- Package.swift
+++ Package.swift
@@ -4,25 +4,28 @@
 import PackageDescription
 
 let package = Package(
-    name: "BottomSheet",
-    products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "BottomSheet",
-            targets: ["BottomSheet"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: "https://github.com/bazinga94/BottomSheet.git", from: "1.0.0"),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "BottomSheet",
-            dependencies: []),
-        .testTarget(
-            name: "BottomSheetTests",
-            dependencies: ["BottomSheet"]),
-    ]
+	name: "BottomSheet",
+	platforms: [
+		.iOS(.v9)
+	],
+	products: [
+		// Products define the executables and libraries a package produces, and make them visible to other packages.
+		.library(
+			name: "BottomSheet",
+			targets: ["BottomSheet"]),
+	],
+	dependencies: [
+		// Dependencies declare other packages that this package depends on.
+		// .package(url: "https://github.com/bazinga94/BottomSheet.git", from: "1.0.0"),
+	],
+	targets: [
+		// Targets are the basic building blocks of a package. A target can define a module or a test suite.
+		// Targets can depend on other targets in this package, and on products in packages this package depends on.
+		.target(
+			name: "BottomSheet",
+			dependencies: []),
+		.testTarget(
+			name: "BottomSheetTests",
+			dependencies: ["BottomSheet"]),
+	]
 )
